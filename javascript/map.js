8c347e0e31412fad82c63d1da2aa77859772ea72
rsanabria11 <rsanabria@hawk.iit.edu>
2024-04-09 23:57:03 -0500

    third feature

--- javascript/map.js
+++ javascript/map.js
@@ -6,17 +6,29 @@ async function initMap() {
   map = new Map(document.getElementById("map"), {
     center: { lat: 34.09011843664449, lng: -118.32688458633045 },
     zoom: 11,
+    center: Hollywood,
   });
   
+  const contentString =
+    '<h1>The Hollywood Sign</h1><p>Spelling out the word "Hollywood" in 50-foot-tall (15.2 m) white uppercase letters and 450 feet (137.2 m) long,[1] it was originally erected in 1923 as a temporary advertisement for a local real estate development, but due to increasing recognition the sign was left up,[2] and replaced in 1978 with a more durable all-steel structure.</p>'
+   
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    ariaLabel: "Hollywood Sign",
+  });
+  const marker = new google.maps.Marker({
+    position: Hollywood,
+    map,
+    title: "Hollywood Sign",
+  });
 
-
-
-
-
-
-
-
-  
+  marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+    });
+  });
+}
   const marker = new google.maps.Marker({
     position: { lat: 34.09011843664449, lng: -118.32688458633045 },
     map: map,
