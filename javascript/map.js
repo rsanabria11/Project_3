897702cfb9ec81002c121d0cd4d16e4f46a18e5d
rsanabria11 <rsanabria@hawk.iit.edu>
2024-04-09 23:46:56 -0500

    addings second  feature

--- javascript/map.js
+++ javascript/map.js
@@ -7,6 +7,21 @@ async function initMap() {
     center: { lat: 34.09011843664449, lng: -118.32688458633045 },
     zoom: 11,
   });
-}
+  
+
+
+
+
+
+
+
+
+  
+  const marker = new google.maps.Marker({
+    position: { lat: 34.09011843664449, lng: -118.32688458633045 },
+    map: map,
+    title: "Los Angeles",
+    draggable: true
+  });
  
 google.maps.event.addDomListener(window, 'load', initMap)
